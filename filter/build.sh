9ecde89b87e85f4a9f984800f522d21786ee562c
Andoni Salcedo Navarro <ansalna@alumni.uv.es>
2023-09-13 12:42:34 +0200

    working on cloudlab

--- filter/build.sh
+++ filter/build.sh
@@ -1,50 +1,14 @@
-# Obtiene el último tag
-latest_tag=$(git describe --tags --abbrev=0)
 
-# Incrementa el número de revisión
-latest_tag=$(git describe --tags --abbrev=0)
+TAG="1.8.0.test"
 
-# Si el tag es del tipo "pre-release" (por ejemplo, 1.0.2-beta)
-if [[ $latest_tag =~ - ]]; then
-    # Divide el número de versión y el sufijo
-    version=$(echo $latest_tag | awk -F- '{print $1}')
-    suffix=$(echo $latest_tag | awk -F- '{print $2}')
+docker build . -t sidecar/filter/controller:$TAG -f deploy/docker/controller/Dockerfile
 
-    # Incrementa el número de versión
-    new_version=$(echo $version | awk -F. '{$NF = $NF + 1;} 1' OFS=.)
+docker tag sidecar/filter/controller:$TAG routerdi1315.uv.es:33443/sidecar/filter/controller:$TAG
 
-    # Combina el nuevo número de versión con el sufijo
-    new_tag="${new_version}-${suffix}"
+docker push routerdi1315.uv.es:33443/sidecar/filter/controller:$TAG
 
-# Si el tag es del tipo "sufijo con punto" (por ejemplo, 1.0.2.test)
-elif [[ $latest_tag =~ \. ]]; then
-    # Divide el número de versión y el sufijo
-    base_version=$(echo $latest_tag | rev | cut -d. -f2- | rev)
-    suffix=$(echo $latest_tag | rev | cut -d. -f1 | rev)
+docker build . -t sidecar/filter/admission:$TAG -f deploy/docker/admission/Dockerfile
 
-    # Verifica si el sufijo es numérico
-    if [[ $suffix =~ ^[0-9]+$ ]]; then
-        # Incrementa el número de versión
-        new_tag=$(echo $latest_tag | awk -F. '{$NF = $NF + 1;} 1' OFS=.)
-    else
-        # Mantiene el mismo sufijo
-        new_tag="${base_version}.${suffix}"
-    fi
+docker tag sidecar/filter/admission:$TAG routerdi1315.uv.es:33443/sidecar/filter/admission:$TAG
 
-else
-    # Incrementa el número de revisión si no hay sufijo
-    new_tag=$(echo $latest_tag | awk -F. '{$NF = $NF + 1;} 1' OFS=.)
-fi
-
-
-docker build . -t sidecar/filter/controller:$new_tag -f deploy/docker/controller/Dockerfile
-
-docker tag sidecar/filter/controller:$new_tag routerdi1315.uv.es:33443/sidecar/filter/controller:$new_tag
-
-docker push routerdi1315.uv.es:33443/sidecar/filter/controller:$new_tag
-
-docker build . -t sidecar/filter/admission:$new_tag -f deploy/docker/admission/Dockerfile
-
-docker tag sidecar/filter/admission:$new_tag routerdi1315.uv.es:33443/sidecar/filter/admission:$new_tag
-
-docker push routerdi1315.uv.es:33443/sidecar/filter/admission:$new_tag
+docker push routerdi1315.uv.es:33443/sidecar/filter/admission:$TAG
