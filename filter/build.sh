fd544317d59d3f8649e61eb5757e10e1299803ca
Juan Gutiérrez-Aguado <juan.gutierrez@uv.es>
2024-08-19 12:57:15 +0200

    Changes in documentation and registry of images

--- filter/build.sh
+++ filter/build.sh
@@ -1,17 +1,20 @@
 #!/bin/bash
 
-TAG="1.10.0.test"
+TAG="1.0.0"
+
+REPO_CONTROLLER="cloudmedialab/filter-controller"
+REPO_ADMISSION="cloudmedialab/filter-admission"
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 
 docker build $SCRIPT_DIR -t sidecar/filter/controller:$TAG -f $SCRIPT_DIR/deploy/docker/controller/Dockerfile
 
-docker tag sidecar/filter/controller:$TAG routerdi1315.uv.es:33443/sidecar/filter/controller:$TAG
+docker tag sidecar/filter/controller:$TAG ${REPO_CONTROLLER}:${TAG}
 
-docker push routerdi1315.uv.es:33443/sidecar/filter/controller:$TAG
+docker push ${REPO_CONTROLLER}:${TAG}
 
 docker build $SCRIPT_DIR -t sidecar/filter/admission:$TAG -f $SCRIPT_DIR/deploy/docker/admission/Dockerfile
 
-docker tag sidecar/filter/admission:$TAG routerdi1315.uv.es:33443/sidecar/filter/admission:$TAG
+docker tag sidecar/filter/admission:$TAG ${REPO_ADMISSION}:${TAG}
 
-docker push routerdi1315.uv.es:33443/sidecar/filter/admission:$TAG
+docker push ${REPO_ADMISSION}:${TAG}
